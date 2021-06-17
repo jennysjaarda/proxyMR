5ad8bd017e9fd02578d84f9f9db33c57f852cec3
jennysjaarda <jennysjaarda@gmail.com>
2021-06-17 13:56:01 +0200

    add account to clustermq

--- _targets.R
+++ _targets.R
@@ -10,8 +10,8 @@ options(clustermq.scheduler = "slurm", clustermq.template = "slurm_clustermq.tmp
 
 tar_option_set(
   resources = tar_resources(
-    clustermq = tar_resources_clustermq(template = list(num_cores = 1,
-                                                        cpus = 1, partition = "cluster2",
+    clustermq = tar_resources_clustermq(template = list(num_cores = 1, account = "sgg"
+                                                        cpus = 1, partition = "sgg",
                                                         log_file="/data/sgg2/jenny/projects/proxyMR/proxymr_%a_clustermq.out"))
   ),
   packages = c("tidyverse", "data.table", "cutr", "ukbtools"),
